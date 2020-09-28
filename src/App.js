9b4aac0f2fc5d0ef1a525294db591e0dad2cb87d
William Jensen <williambjensen01@gmail.com>
2020-09-27 18:27:14 -0600

    Completed MVP and some stretch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Character from './components/Character';
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then((response) => {
+        console.log(response.data.results)
+        setData(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, []);
+    
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <Character characterArray={data} />
     </div>
   );
 }
